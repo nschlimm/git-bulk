50dcd4c5c2ad73a8292c5f5ef8a8595721fe6c8f
Niklas Schlimm <niklas.schlimm@provinzial.com>
2017-03-08 16:33:06 +0100

    tabs deleted

--- git-bulk.sh
+++ git-bulk.sh
@@ -19,75 +19,75 @@ usage() {
 
 # add another workspace to global git config
 function addWSDir () {
-	git config --global bulkworkspaces.$wsname "$wsdir"
+  git config --global bulkworkspaces.$wsname "$wsdir"
 }
 
 # add current directory
 function addCurrWSDir () {
-	git config --global bulkworkspaces.$wsname "$PWD"
+  git config --global bulkworkspaces.$wsname "$PWD"
 }
 
 # remove workspace from global git config
 function remWSDir () {
-	git config --global --unset bulkworkspaces.$wsname
+  git config --global --unset bulkworkspaces.$wsname
 }
 
 # remove workspace from global git config
 function purgeWS () {
-	git config --global --remove-section bulkworkspaces
+  git config --global --remove-section bulkworkspaces
 }
 
 # list all current workspace locations defined
 function listWSDir () {
-	git config --global --get-regexp bulkworkspaces
+  git config --global --get-regexp bulkworkspaces
 }
 
 # atomic execution of a git command in one specific repository
 function guardedExecution () {
-   if [[ $guarded ]]; then
-     echo -n "${invers}git $gitcommand${reset} -> execute here (y/n)? "
-     read -n 1 -r </dev/tty; echo
-     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        atomicExecution
-     fi
-   else
+  if [[ $guarded ]]; then
+    echo -n "${invers}git $gitcommand${reset} -> execute here (y/n)? "
+    read -n 1 -r </dev/tty; echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      atomicExecution
+    fi
+  else
      atomicExecution
-   fi
+  fi
 }
 
 # execute a git command with log
 function atomicExecution () {
-	echo "${bldred}->${reset} executing ${invers}git $gitcommand${reset}"
-	git $gitcommand
+  echo "${bldred}->${reset} executing ${invers}git $gitcommand${reset}"
+  git $gitcommand
 }
 
 function checkGitCommand () {
-	if git help -a | cat | grep -o "$corecommand"; then
-		echo "Core command $corecommand accepted."
-	else
-		usage
-		echo "error: unknown GIT command: $corecommand"
-		exit 1
-	fi
+  if git help -a | cat | grep -o "$corecommand"; then
+    echo "Core command $corecommand accepted."
+  else
+    usage
+    echo "error: unknown GIT command: $corecommand"
+    exit 1
+  fi
 }
 
 # execute the bul operation
 function executBulkOp () {
-	clear
-	listWSDir | while read workspacespec; do
-	   wslocation=$(echo $workspacespec | cut -f2 -d' ')
-	   eval cd "$wslocation"
-	   actual=$(pwd)
-	   echo "Executing bulk operation in workspace ${invers}$actual${reset}"
-       eval find . -name ".git" | while read line; do
-       	  gitrepodir=${line::${#line}-5} # cut the .git part of find results to have the root git directory of that repository
-	      eval cd "$gitrepodir" # into git repo location
-	      curdir=$(pwd)
-	      echo "Current repository: ${curdir%/*}/${bldred}${curdir##*/}${reset}"
-	        guardedExecution
-	      eval cd "$wslocation" # back to origin location of last find command
-       done 
-	done 
+  clear
+  listWSDir | while read workspacespec; do
+    wslocation=$(echo $workspacespec | cut -f2 -d' ')
+    eval cd "$wslocation"
+    actual=$(pwd)
+    echo "Executing bulk operation in workspace ${invers}$actual${reset}"
+    eval find . -name ".git" | while read line; do
+      gitrepodir=${line::${#line}-5} # cut the .git part of find results to have the root git directory of that repository
+      eval cd "$gitrepodir" # into git repo location
+      curdir=$(pwd)
+      echo "Current repository: ${curdir%/*}/${bldred}${curdir##*/}${reset}"
+      guardedExecution
+      eval cd "$wslocation" # back to origin location of last find command
+    done 
+  done 
 }
 
 initialcount="${#}"
